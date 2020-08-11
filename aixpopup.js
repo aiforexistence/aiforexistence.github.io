5f200aad318d1db232837927f62be8b93c16790c
reginz <53823830+reginz@users.noreply.github.com>
2020-08-11 09:31:45 +0300

    Add files via upload

---
+++ aixpopup.js
@@ -0,0 +1,4 @@
+document.getElementById('join-button').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+document.queryCommandEnabled.querySelector('.close').addEventListener
